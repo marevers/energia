69ce73c2e8bbf20282019118bca628964d9615f6
nithia <nithia@mindworks.software>
2019-01-04 09:56:53 +0200

    Test basic queries

--- src/hid-test.go
+++ src/hid-test.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/dbld-org/energia/src/axpert"
 	"github.com/kristoiv/hid"
 )
-
 
 func main() {
 	devs, err := hid.Devices()
@@ -18,16 +18,35 @@ func main() {
 		di = dev
 	}
 
-
-	fmt.Println("Opening USB device ", di.Path)
-	d, err := di.Open()
+	conn, err := axpert.NewUSBConnector(di.Path)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Reading from device" )
-	ch := d.ReadCh()
-	bytes := <- ch
-	fmt.Println(bytes)
+	fmt.Println("Connecting to ", conn.Path())
+	err = conn.Open()
+	if err != nil {
+		panic(err)
+	}
 
+	protocolId, err := axpert.ProtocolId(conn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ProtocolId: ", protocolId)
+
+	serialNo, err := axpert.SerialNo(conn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("SerialNo: ", serialNo)
+
+	version, err := axpert.InverterFirmwareVersion(conn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("FirmwareVersion: ", version)
+
+	fmt.Println("Closing connection")
+	conn.Close()
 }
