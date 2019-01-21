b052f8d3a7a5a46b2c2ac21e575ed7e3217f12a2
nithia <nithia@mindworks.software>
2019-01-21 19:08:48 +0200

    basic data log daemon

--- src/datalogd.go
+++ src/datalogd.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/eclipse/paho.mqtt.golang"
 
 	"github.com/dbld-org/energia/src/axpert"
 )
@@ -14,41 +17,100 @@ import (
 const path = "/dev/hidraw0"
 const interval = 30 * time.Second
 
-//const mqttServer = "192.168.24.39"
-//const mqttPort = 1883
+const mqttServer = "192.168.24.39"
+const mqttPort = "1883"
+const mqttTopic = "axpert/data"
+
+type messageData struct {
+	Timestamp time.Time
+	Data      interface{}
+}
 
 func main() {
+
+	uc, err := axpert.NewUSBConnector(path)
+	if err != nil {
+		panic(err)
+	}
+	defer uc.Close()
+	fmt.Println("connected to ", path)
+
+	clientOpts := mqtt.NewClientOptions()
+	clientOpts.AddBroker("tcp://" + mqttServer + ":" + mqttPort)
+	clientOpts.SetAutoReconnect(true)
+	clientOpts.SetStore(mqtt.NewFileStore("/tmp/mqtt"))
+	clientOpts.SetCleanSession(false)
+	clientOpts.SetClientID("Axpert")
+
+	client := mqtt.NewClient(clientOpts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	defer client.Disconnect(250)
+	fmt.Println("Connected to mqtt")
 
 	ticker := time.NewTicker(interval)
 
 	go func() {
-		uc, err := axpert.NewUSBConnector(path)
-		if err != nil {
-			panic(err)
-		}
-		defer uc.Close()
-		fmt.Println("connected to ", path)
-
 		for t := range ticker.C {
 			mode, err := axpert.DeviceMode(uc)
 			if err != nil {
 				panic(err)
 			}
+			m := map[string]string{"Mode": mode}
+			msgData := messageData{Timestamp: t, Data: m}
+			err = sendMessage(msgData, client)
+			if err != nil {
+				panic(err)
+			}
+
 			status, err := axpert.DeviceGeneralStatus(uc)
 			if err != nil {
 				panic(err)
 			}
-			// TODO more calls, send to mqtt
-			fmt.Println(t.Format(time.RFC3339), " mode: ", mode, " status: ", status)
+			msgData = messageData{Timestamp: t, Data: status}
+			err = sendMessage(msgData, client)
+			if err != nil {
+				panic(err)
+			}
+
+			warnings, err := axpert.WarningStatus(uc)
+			if err != nil {
+				panic(err)
+			}
+			msgData = messageData{Timestamp: t, Data: warnings}
+			err = sendMessage(msgData, client)
+			if err != nil {
+				panic(err)
+			}
+
+			flags, err := axpert.DeviceFlagStatus(uc)
+			msgData = messageData{Timestamp: t, Data: flags}
+			err = sendMessage(msgData, client)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(t.Format(time.RFC3339), " mode: ", mode, " status: ", status, " flags: ", flags, " warnings: ", warnings)
 		}
-		fmt.Println("closing connection")
 	}()
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	sig := <-sigChan
+	close(sigChan)
 	fmt.Println(sig, " stopping ticker")
 	ticker.Stop()
 
 	fmt.Println("exiting")
+}
+
+func sendMessage(data messageData, client mqtt.Client) error {
+	msg, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	token := client.Publish(mqttTopic, 1, false, msg)
+	token.Wait()
+	return nil
 }
